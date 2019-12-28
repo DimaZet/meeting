441bc96dd6930cab9e70fa159221437df3aae0ff
Заитов Дмитрий <zaitov-dmitry@yandex-team.ru>
2019-12-29 02:34:11 +0300

    add lombok
    add tests and mongo embedded in them
    
    add mongo event listener for setting createdAt

--- src/test/java/ru/party/meeting/MeetingApplicationTests.java
+++ src/test/java/ru/party/meeting/MeetingApplicationTests.java
@@ -1,13 +1,42 @@
 package ru.party.meeting;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class MeetingApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void testPostingImage() throws Exception {
+		String title = "let's go cinema";
+		String description = "tomorrow i'd like to go cinema with girl";
+		this.mockMvc.perform(post("/events?title={title}&description={description}", title, description))
+				.andDo(print())
+				.andExpect(status().is(200))
+				.andExpect(jsonPath("id").isNotEmpty())
+				.andExpect(jsonPath("title").value(title))
+				.andExpect(jsonPath("description").value(description))
+				.andExpect(jsonPath("createdAt").isNotEmpty());
+	}
+
+	@Test
+	void testGetImages() throws Exception {
+		this.mockMvc.perform(get("/events"))
+				.andDo(print())
+				.andExpect(status().is(200));
 	}
 
 }
