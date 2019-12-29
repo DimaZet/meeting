8064e727e6471f8fea9e42e9fdc63a9c3de3e302
Заитов Дмитрий <45773616+DimaZet@users.noreply.github.com>
2019-12-29 11:50:59 +0300

    lombok + base tests with mongo (#2)
    * add lombok
    
    add tests and mongo embedded in them
    
    add mongo event listener for setting createdAt
    
    * rename tests and add .mvn in gitignore

--- src/main/java/ru/party/meeting/model/MeetingEvent.java
+++ src/main/java/ru/party/meeting/model/MeetingEvent.java
@@ -3,39 +3,31 @@ package ru.party.meeting.model;
 import java.time.Instant;
 import java.util.UUID;
 
-import org.springframework.data.annotation.CreatedDate;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document
 public class MeetingEvent {
 
     @Id
-    private UUID id;
+    @Getter
+    private final UUID id;
+    @Getter
+    @Setter
     private String title;
+    @Getter
+    @Setter
     private String description;
+
+    @Getter
+    @Setter //@CreatedDate TODO auditor
+    private Instant createdAt;
 
     public MeetingEvent(UUID id, String title, String description) {
         this.id = id;
         this.title = title;
         this.description = description;
-    }
-
-    @CreatedDate
-    private Instant createdAt;
-
-
-    public UUID getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
     }
 }
