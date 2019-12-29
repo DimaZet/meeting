510aede4cdc125f858a75318987eaf3190e6651e
Заитов Дмитрий <zaitov-dmitry@yandex-team.ru>
2019-12-29 23:43:28 +0300

    style fixes after evgenys

--- src/main/java/ru/party/meeting/controller/HTMLMeetingEventController.java
+++ src/main/java/ru/party/meeting/controller/HTMLMeetingEventController.java
@@ -1,16 +1,11 @@
 package ru.party.meeting.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import ru.party.meeting.model.MeetingEvent;
 import ru.party.meeting.service.MeetingEventService;
-
-import java.util.List;
 
 @Controller
 public class HTMLMeetingEventController {
@@ -23,7 +18,6 @@ public class HTMLMeetingEventController {
 
     @GetMapping("/events")
     public String getAllEvents(Model model) {
-
         model.addAttribute("eventsList", meetingEventService.getAllEvents());
         model.addAttribute("added", false);
         return "events-list";
@@ -31,11 +25,11 @@ public class HTMLMeetingEventController {
 
     @PostMapping("/events")
     public String createEvent(
-            @RequestParam(name = "title") String title, @RequestParam(name = "description") String description, Model model) {
+            @RequestParam(name = "title") String title, @RequestParam(name = "description") String description,
+            Model model) {
         meetingEventService.createEvent(title, description);
         model.addAttribute("eventsList", meetingEventService.getAllEvents());
         model.addAttribute("added", true);
         return "events-list";
     }
-
 }
