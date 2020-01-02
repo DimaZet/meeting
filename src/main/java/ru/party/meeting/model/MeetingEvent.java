fc691a9c9d70b0f4f11ad43b619adc7953032c29
Evgeny <evsmanko@gmail.com>
2020-01-02 13:49:16 +0300

    @Field annotation required by Spring Documentation 2.1

--- src/main/java/ru/party/meeting/model/MeetingEvent.java
+++ src/main/java/ru/party/meeting/model/MeetingEvent.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document
 public class MeetingEvent {
@@ -14,15 +15,20 @@ public class MeetingEvent {
     @Id
     @Getter
     private final UUID id;
+
     @Getter
     @Setter
+    @Field
     private String title;
+
     @Getter
     @Setter
+    @Field
     private String description;
 
     @Getter
     @Setter //@CreatedDate TODO auditor
+    @Field
     private Instant createdAt;
 
     public MeetingEvent(UUID id, String title, String description) {
